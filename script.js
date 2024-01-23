f3220d33e99c0bf8914e2df87e51006c76cd27d4
micha <michael.sm@live.co.uk>
2024-01-23 18:41:50 +0000

    updating javascript code

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Wait for the document to be ready
 $(document).ready(function() {
     // Get the current date and display it at the top of the calendar
-    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
     $('#currentDay').text(currentDate);
   
-    // Loop to dynamically generate timeblocks for standard business hours
     for (let hour = 9; hour <= 17; hour++) {
       // Create timeblock elements (e.g., divs for each hour)
       const timeblock = $('<div>').addClass('time-block row');
@@ -12,7 +11,7 @@ $(document).ready(function() {
       const eventInput = $('<textarea>').addClass('description col-md-10');
       const saveButton = $('<button>').addClass('saveBtn col-md-1').html('<i class="fas fa-save"></i>');
   
-      // Add classes for past, present, or future timeblocks
+      // Classes for past, present, or future timeblocks
       if (hour < dayjs().hour()) {
         timeblock.addClass('past');
       } else if (hour === dayjs().hour()) {
@@ -21,7 +20,7 @@ $(document).ready(function() {
         timeblock.addClass('future');
       }
   
-      // Add event listeners for saving events
+      // Event listener for saving events
       saveButton.on('click', function() {
         const eventText = eventInput.val();
         localStorage.setItem(`event-${hour}`, eventText);
